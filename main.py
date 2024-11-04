1ce964cc597fa29000b0ac3745af9b038ab4f019
doublespiral <113387397+doublespiral@users.noreply.github.com>
2024-11-03 19:19:57 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -10,11 +10,15 @@ with open("secrets.json") as secrets_file:
     secrets = load_json( secrets_json )
 
     secrets_file.close()
-    #print(secrets["token"])
 
 
 TOKEN: Final[str] = secrets["token"]
-ENFORCED_CHANNEL_ID: Final[int] = 1300928626392301750
+
+CHANNEL_RULES: Final[int] = 1300928626392301750
+CHANNEL_TRASH: Final[int] = 1302776301161287752
+
+BOT_PANCAKE: Final[int] = 239631525350604801
+
 
 
 # bot setup
@@ -24,13 +28,19 @@ intents.message_content = True
 client: Client = Client(intents=intents)
 
 
+def bad_logging(message: Message) -> None:
+    username: str = str(message.author)
+    channel: str = str(message.channel)
+    print(f"[{channel}] {username}: \"{message.content}\"")
+    return
 
-def enforced_channel(channel_id: int) -> bool:
-    return ( channel_id == ENFORCED_CHANNEL_ID)
+
+def in_rules_channel(channel_id: int) -> bool:
+    return (channel_id == CHANNEL_RULES)
 
 
 async def get_last_rule_num(message: Message) -> int:
-    channel = client.get_channel( ENFORCED_CHANNEL_ID )
+    channel = client.get_channel( CHANNEL_RULES )
 
     messages = []
 
@@ -69,17 +79,31 @@ async def formatted(message: Message) -> bool:
 
 
 # message functionality
-async def validate_message(message: Message) -> None:
+async def check_rule(message: Message) -> None:
+    #bad_logging(message)
+
     if not message.content:
         print("Message probably because intents were not enabled")
         return
 
-    if not enforced_channel( message.channel.id ):
-        return
+
 
     if not ( await formatted(message) ):
         await message.delete()
+
+    return
+
+
+async def move_output(message: Message) -> None:
+    message.content = "Moved pancake message: \n" + message.content
     
+    trash = client.get_channel(CHANNEL_TRASH)
+    if (len(message.embeds) > 0):
+        await trash.send(message.content, embed=message.embeds[0])
+    else:
+        trash.send(message.content)
+
+    await message.delete()
     return
 
 
@@ -91,17 +115,17 @@ async def on_ready() -> None:
 
 @client.event 
 async def on_message(message: Message) -> None:
-    
-    # if message is from bot, ignore
+    # if message is from our bot, ignore
     if (message.author == client.user):
-        return 
+        return
+
+    bad_logging(message)
+
+    if (message.author.id == BOT_PANCAKE):
+        await move_output(message)
     
-    username: str = str(message.author)
-    channel: str = str(message.channel)
-
-    print(f"[{channel}] {username}: \"{message.content}\"")
-
-    await validate_message(message)
+    if in_rules_channel( message.channel.id ):
+        await check_rule(message)
 
     return
 
